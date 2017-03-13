04993dc606b242cf316516976904d42f8f893668
jbrzakala <jbrzakala@student.tgm.ac.at>
2017-03-13 10:32:36 +0100

    punkte

--- src/julian/hallo7.java
+++ src/julian/hallo7.java
@@ -7,7 +7,7 @@ public class hallo7 {
 	}
 	
 	public static void byby(){
-		System.out.println("Auf Wiedersehen");
+		System.out.println("Auf Wiedersehen...");
 	}
 	
 	public static void main(String[] args){
